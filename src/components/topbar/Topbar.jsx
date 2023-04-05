acfa23c6c8a8139b7b5e6ed1b619a2a1b344a142
yıldız <mesel4461@gmail.com>
2023-04-06 02:12:45 +0300

    as

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./topbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import { Chat, Notifications } from "@mui/icons-material";
+const Topbar = () => {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Lamasocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input
+            type="text"
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <div className="topbarLink">Homepage</div>
+          <div className="topbarLink">Timeline</div>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="./assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Topbar from "../../components/topbar/Topbar";
+import "./home.css";
+
+const Home = () => {
+  return <Topbar />;
+};
+
+export default Home;
