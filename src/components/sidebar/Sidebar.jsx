1b72630f45d36a95da6c6002e1c7a3ac6366c14b
yıldız <mesel4461@gmail.com>
2023-04-06 13:29:37 +0300

    as

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,48 @@
 import React from "react";
 import "./sidebar.css";
+import RssFeedIcon from "@mui/icons-material/RssFeed";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <ul className="sidebarList">
-          <li className="sidebarListItem"></li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
         </ul>
       </div>
     </div>
